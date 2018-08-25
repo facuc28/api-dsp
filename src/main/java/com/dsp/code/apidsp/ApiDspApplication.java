abdf39385ce7bce6161a651d9dacc675ab9da3d9
Facundo Crusta <Facundo.Crusta@wnco.com>
2018-08-25 14:38:22 -0300

    Facundo Crusta - Added gradle config

---
+++ src/main/java/com/dsp/code/apidsp/ApiDspApplication.java
@@ -0,0 +1,12 @@
+package com.dsp.code.apidsp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDspApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiDspApplication.class, args);
+	}
+}
